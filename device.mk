dfc9161f6d634435158196a2ab7085a01d79446c
Rishi Roy <66902810+R15Hi@users.noreply.github.com>
2022-11-01 18:10:25 +0530

    RMX1851: Move performance Configs to dedicated folder
    Signed-off-by: Rishi Roy <66902810+R15Hi@users.noreply.github.com>
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- device.mk
+++ device.mk
@@ -322,7 +322,7 @@ PRODUCT_PACKAGES += \
 
 # IRQBalance
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/msm_irqbalance.conf
 
 # Keymaster
 PRODUCT_PACKAGES += \
@@ -406,7 +406,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/perf/powerhint.json
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
