e95dcadf6f4a27b6836abd5ce00f2fcc00a03873
Cykeek <Cykeek@proton.me>
2022-12-07 11:10:27 +0530

    RMX1851: bringup Naika dependencies * removed latest sepolicy changes
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1851.mk
+    $(LOCAL_DIR)/naika_RMX1851.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX1851-user \
-    lineage_RMX1851-userdebug \
-    lineage_RMX1851-eng
+    naika_RMX1851-user \
+    naika_RMX1851-userdebug \
+    naika_RMX1851-eng
--- naika_RMX1851.mk
+++ naika_RMX1851.mk
@@ -6,11 +6,11 @@
 
 $(call inherit-product, device/realme/RMX1851/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common NaikaOS stuff.
+$(call inherit-product, vendor/naika/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_RMX1851
+PRODUCT_NAME := naika_RMX1851
 PRODUCT_DEVICE := RMX1851
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := realme 3 Pro
@@ -28,7 +28,7 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 BUILD_FINGERPRINT := google/cheetah/cheetah:13/TD1A.221105.001/9104446:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cheetah-user 13 TD1A.221105.001 9104446 release-keys"
+    PRIVATE_BUILD_DESC="cheetah-user 13 TD1A.221105.001 9104446 release-keys" \
     PRODUCT_NAME="RMX1851"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
