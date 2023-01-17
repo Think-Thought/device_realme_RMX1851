73514194ec5c832b67990ac284ad0058907ed11e
Cykeek <Cykeek@proton.me>
2023-01-17 15:51:04 +0530

    RMX1851: Remove tracking vendor from Cykeek-labs
    * It's private as of now!!
    
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,13 +13,7 @@ echo "================================"
 
 banner 2>1
 
-# Vendor
-banner 2>1
-echo "Clonning Vendor..."
-git clone https://github.com/Cykeek-Labs/vendor_realme_RMX1851 -b 13 vendor/realme/RMX1851
-
 # Kernel
-
 banner 2>1
 echo "Cloning Kernel..."
 git clone https://gitlab.com/arrowos-project/android_prebuilts_clang_host_linux-x86_clang-r437112b prebuilts/clang/host/linux-x86/clang-r437112b
