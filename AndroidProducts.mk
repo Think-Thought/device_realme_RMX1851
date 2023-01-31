38f2f2fb7a6d4cf80c65a5645cacec39d5182fd1
Think_Thought <123951266+Think-Thought@users.noreply.github.com>
2023-01-31 11:27:18 +0530

    RMX1851: BananaDroid bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1851.mk
+    $(LOCAL_DIR)/banana_RMX1851.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX1851-user \
-    lineage_RMX1851-userdebug \
-    lineage_RMX1851-eng
+    banana_RMX1851-user \
+    banana_RMX1851-userdebug \
+    banana_RMX1851-eng
