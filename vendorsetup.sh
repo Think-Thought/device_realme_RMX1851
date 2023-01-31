37851c0e256fab20f3975e713f909838b3ba3b55
Think_Thought <123951266+Think-Thought@users.noreply.github.com>
2023-01-31 12:00:31 +0530

    RMX1851: Update links

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 
-banner(){
-clear
-echo "================================"
-echo "|                              |"
-echo "|   Realme 3 Pro Setup Script  |"
-echo "|       Branch: Lineage        |"
-echo "|    Maintained By: Cykeek     |"
-echo "|                              |"
-echo "================================"
-}
+# Vendor
+echo "Clonning Vendor..."
+git clone https://github.com/kssrao13882/android_vendor_realme_sdm710-common -b 13 vendor/realme/sdm710-common
 
-banner 2>1
+# Device
+echo "Clonning common device tree..."
+git clone https://github.com/kssrao13882/device_realme_sdm710-common_A13 -b 13 device/realme/sdm710-common
 
 # Kernel
-banner 2>1
 echo "Cloning Kernel..."
 git clone https://gitlab.com/arrowos-project/android_prebuilts_clang_host_linux-x86_clang-r437112b prebuilts/clang/host/linux-x86/clang-r437112b
-git clone https://github.com/Cykeek-Labs/kernel_realme_sdm710 kernel/realme/sdm710
-
+git clone https://github.com/U89-sk/viviz-sdm710 kernel/realme/sdm710
 
 # RealmeParts
-banner 2>1
 echo "Cloning RealmeParts..."
 git clone https://github.com/Cykeek-Labs/packages_apps_realmeparts packages/apps/RealmeParts
