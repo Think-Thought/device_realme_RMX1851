8dec9cf020a97b3eef01f1ff5c81856b1fba8d58
Think_Thought <123951266+Think-Thought@users.noreply.github.com>
2023-02-02 03:18:05 +0530

    RMX1851: Change Kernel link

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,8 +2,8 @@
 
 # Kernel
 echo "Cloning Kernel..."
-#git clone https://gitlab.com/arrowos-project/android_prebuilts_clang_host_linux-x86_clang-r437112b prebuilts/clang/host/linux-x86/clang-r437112b
-git clone https://github.com/eun0115/kernel_realme_sdm710 -b staging kernel/realme/sdm710
+git clone https://gitlab.com/arrowos-project/android_prebuilts_clang_host_linux-x86_clang-r437112b prebuilts/clang/host/linux-x86/clang-r437112b
+git clone https://github.com/Think-Thought/kernel_realme_sdm710 kernel/realme/sdm710
 
 # RealmeParts
 echo "Cloning RealmeParts..."
