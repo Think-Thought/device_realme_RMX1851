653902560c45052414ab3a755dfc03b1eeeec2e1
Think_Thought <123951266+Think-Thought@users.noreply.github.com>
2023-02-01 20:47:12 +0530

    RMX1851: add # in link

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@
 
 # Kernel
 echo "Cloning Kernel..."
-git clone https://gitlab.com/arrowos-project/android_prebuilts_clang_host_linux-x86_clang-r437112b prebuilts/clang/host/linux-x86/clang-r437112b
+#git clone https://gitlab.com/arrowos-project/android_prebuilts_clang_host_linux-x86_clang-r437112b prebuilts/clang/host/linux-x86/clang-r437112b
 git clone https://github.com/eun0115/kernel_realme_sdm710 -b staging kernel/realme/sdm710
 
 # RealmeParts
