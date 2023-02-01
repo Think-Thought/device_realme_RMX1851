8f343605e740f100548a2439f37ba320cf2cc9ae
Think_Thought <123951266+Think-Thought@users.noreply.github.com>
2023-02-01 18:39:16 +0530

    RMX1851: Remove comman links

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Vendor
-echo "Clonning Vendor..."
-git clone https://gitlab.com/Think-Thought/android_vendor_realme_sdm710-common vendor/realme/sdm710-common
-
-# Device
-echo "Clonning common device tree..."
-git clone https://gitlab.com/Think-Thought/device_realme_sdm710-common -b 13 device/realme/sdm710-common
-
 # Kernel
 echo "Cloning Kernel..."
 git clone https://gitlab.com/arrowos-project/android_prebuilts_clang_host_linux-x86_clang-r437112b prebuilts/clang/host/linux-x86/clang-r437112b
